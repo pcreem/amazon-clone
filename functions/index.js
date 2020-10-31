c8c4bd8682599277d4713ed65fc46b7b6622010a
pcreem <creempepper@gmail.com>
2020-10-31 15:33:51 +0800

    payment before firebase upgrade

--- functions/index.js
+++ functions/index.js
@@ -16,6 +16,23 @@ app.use(express.json());
 // - API routes
 app.get("/", (request, response) => response.status(200).send("hello world"));
 
+app.post("/payments/create", async (request, response) => {
+    const total = request.query.total;
+  
+    
+  
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total, // subunits of the currency
+      currency: "usd",
+    });
+  
+    // OK - Created
+    response.status(201).send({
+      clientSecret: paymentIntent.client_secret,
+    });
+    console.log("Payment Request Recieved BOOM!!! for this amount >>> ", total);
+  });
+  
 // - Listen command
 exports.api = functions.https.onRequest(app);
 
--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,15 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDw3QXKLIgjSxduHIj4Ih7lEnuC8eFKS70",
-    authDomain: "clone-c7ffb.firebaseapp.com",
-    databaseURL: "https://clone-c7ffb.firebaseio.com",
-    projectId: "clone-c7ffb",
-    storageBucket: "clone-c7ffb.appspot.com",
-    messagingSenderId: "443096472787",
-    appId: "1:443096472787:web:a0f92136c552a6a6ae8826",
-    measurementId: "G-Y32BLPKCKN"
-  };
+  apiKey: "AIzaSyDw3QXKLIgjSxduHIj4Ih7lEnuC8eFKS70",
+  authDomain: "clone-c7ffb.firebaseapp.com",
+  databaseURL: "https://clone-c7ffb.firebaseio.com",
+  projectId: "clone-c7ffb",
+  storageBucket: "clone-c7ffb.appspot.com",
+  messagingSenderId: "443096472787",
+  appId: "1:443096472787:web:a0f92136c552a6a6ae8826",
+  measurementId: "G-Y32BLPKCKN"
+};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
